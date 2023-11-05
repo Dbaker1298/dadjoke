db8be56fb519a861f68b8f93ce25acd60210396b
David Baker <dpbaker1298@gmail.com>
2023-11-05 12:34:25 -0500

    refactor short and long descriptions

---
+++ cmd/random.go
@@ -0,0 +1,37 @@
+/*
+Copyright © 2023 David Baker
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// randomCmd represents the random command
+var randomCmd = &cobra.Command{
+	Use:   "random",
+	Short: "GET a random dad joke",
+	Long: `This command will GET a random dad joke from icanhazdadjoke.com and print it to your terminal.
+
+Dad jokes are essential to life and children's development.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("random called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(randomCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// randomCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// randomCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
