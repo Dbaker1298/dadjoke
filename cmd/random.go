1bf671627e944a601a5dc3eaf2c3b32f3cafe862
David Baker <dpbaker1298@gmail.com>
2023-11-05 14:52:25 -0500

    Add support for search term in getRandomJoke function

--- cmd/random.go
+++ cmd/random.go
@@ -71,20 +71,21 @@ var randomCmd = &cobra.Command{
 
 Dad jokes are essential to life and children's development.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		getRandomJoke()
+		jokeTerm, _ := cmd.Flags().GetString("term")
+
+		if jokeTerm != "" {
+			getRandomJokeWithTerm(jokeTerm)
+		} else {
+			getRandomJoke()
+		}
 	},
+}
+
+func getRandomJokeWithTerm(term string) {
+	fmt.Println("Term: ", term)
 }
 
 func init() {
 	rootCmd.AddCommand(randomCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// randomCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// randomCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+		randomCmd.PersistentFlags().String("term", "", "A search term to find a Dad joke with.")
 }
